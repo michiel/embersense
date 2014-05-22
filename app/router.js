3830bba16772432f49ee9be4b8d2d9125b7b7b14
Michiel Kalkman <michiel@nosuchtype.com.com>
2014-05-22 13:10:34 +0200

    Remove component and helper routes

--- app/router.js
+++ app/router.js
@@ -3,8 +3,6 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 Router.map(function() {
   this.route('about');
   this.route('login');
-  this.route('component-test');
-  this.route('helper-test');
   this.resource('sensors', function() {
       this.resource('sensor', { path:'/:sensor_id' }, 
         function() {
