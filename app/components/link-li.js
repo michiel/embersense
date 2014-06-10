d7546990e7868efddb470db2277d4d535d7c70d3
Michiel Kalkman <michiel@michielkalkman.com>
2014-06-10 13:14:32 +0200

    Add link-li component

---
+++ app/components/link-li.js
@@ -0,0 +1,8 @@
+export default Ember.Component.extend({
+    tagName           : 'li',
+    classNameBindings : ['active'],
+
+    active: function() {
+      return this.get('childViews').anyBy('active');
+    }.property('childViews.@each.active')
+});
